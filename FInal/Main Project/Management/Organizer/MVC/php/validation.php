bb2c70b82a1db1a29e24a9b39c0eca47119087a8
rahmanmushiuraiub <mushiurrehman1610917670@gmail.com>
2026-01-01 20:09:32 +0600

    project

---
+++ FInal/Main Project/Management/Organizer/MVC/php/validation.php
@@ -0,0 +1,47 @@
+<?php
+include "db/db.php";
+$success = "";
+$error = "";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $fullname = trim($_POST['fullname']);
+    $phone = trim($_POST['phone']);
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    
+    if(empty($fullname) || empty($phone) || empty($email) || empty($password)){
+        die("All fields are required");
+    }
+
+    if(preg_match('/[0-9]/', $fullname)){
+        die("Name must not contain numbers");
+    }
+
+    if(!preg_match('/^01[0-9]{9}$/', $phone)){
+        die("Please enter a valid phone number");
+    }
+
+    if(!preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/", $email)){
+        die("Please enter a valid email address (e.g., example@domain.com)");
+    }
+
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO Admin (fullname, phone, email, password, verified) 
+        VALUES ('$fullname', '$phone', '$email', '$hashedPassword', 1)";
+
+ if (mysqli_query($conn, $sql)) {
+        $_SESSION['fullname'] = $fullname;
+         $success = "Registration successful!";  
+        header("Location: landinpage.php");
+        exit;
+    } else {
+        echo "Database error: " . mysqli_error($conn);
+    }
+    $conn->close();
+}else {
+    echo "Invalid request";
+    
+
+}
+?>
