cab48dd11cc8afbbe031429fc17dd9cfb991aa2b
rahmanmushiuraiub <mushiurrehman1610917670@gmail.com>
2026-01-04 14:06:53 +0600

    done

--- FInal/Main Project/Management/Organizer/MVC/php/validation.php
+++ FInal/Main Project/Management/Organizer/MVC/php/validation.php
@@ -1,48 +1,51 @@
 <?php
 include "../db/db.php";
+ 
 $success = "";
 $error = "";
+ 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-$fullname = htmlspecialchars(trim($_POST['fullname']));
-$phone    = htmlspecialchars(trim($_POST['phone']));
-$email    = htmlspecialchars(trim($_POST['email']));
-$password = htmlspecialchars(trim($_POST['password']));
-
-
-    
+    $fullname = trim($_POST['fullname']);
+    $phone = trim($_POST['phone']);
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+ 
+   
     if(empty($fullname) || empty($phone) || empty($email) || empty($password)){
         die("All fields are required");
     }
-
+ 
     if(preg_match('/[0-9]/', $fullname)){
         die("Name must not contain numbers");
     }
-
+ 
     if(!preg_match('/^01[0-9]{9}$/', $phone)){
         die("Please enter a valid phone number");
     }
-
+ 
     if(!preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/", $email)){
         die("Please enter a valid email address (e.g., example@domain.com)");
     }
-
+ 
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-$sql = "INSERT INTO admin (fullname, phone, email, password, verified) 
-        VALUES ('$fullname', '$phone', '$email', '$hashedPassword', 1)";
-
- if (mysqli_query($conn, $sql)) {
-        $_SESSION['fullname'] = $fullname;
-         $success = "Registration successful!";  
-        header("Location:html/landinpage.php");
-        exit;
+    $sql = "INSERT INTO admin (fullname, phone, email, password, verified)
+        VALUES ('$fullname',   '$phone', '$email', '$hashedPassword', '1')";
+ 
+    if ($conn->query($sql)) {
+        $success = "Registration successful!";  
+        header("Location: ../php/login.php");
+        exit();
+ 
     } else {
-        echo "Database error: " . mysqli_error($conn);
+        $error="ERROR ". $conn->error;
     }
+ 
+ 
     $conn->close();
-}else {
-    echo "Invalid request";
-    
-
+    echo $success;
+    echo $error;    
+   
+ 
+ 
 }
 ?>
