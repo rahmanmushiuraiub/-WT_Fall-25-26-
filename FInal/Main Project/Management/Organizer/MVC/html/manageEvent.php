b072169bf74d0cc7e21b1cfa6a6a5ab365f710c5
rahmanmushiuraiub <mushiurrehman1610917670@gmail.com>
2026-01-10 11:16:27 +0600

    manageevent

---
+++ FInal/Main Project/Management/Organizer/MVC/html/manageEvent.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pending Event Requests</title>
+    <link rel="stylesheet" href="../css/manageEvent.css">
+</head>
+<body>
+    
+
+    <h2>Pending Event Requests</h2>
+
+    
+    <?php include "../php/manageEvent.php"; ?>
+</body>
+</html>
