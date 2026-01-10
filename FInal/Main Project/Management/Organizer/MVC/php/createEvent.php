03ae049aa02366195165cfb323fe0234ad02eaed
rahmanmushiuraiub <mushiurrehman1610917670@gmail.com>
2026-01-10 11:06:33 +0600

    create event backend

--- FInal/Main Project/Management/Organizer/MVC/php/createEvent.php
+++ FInal/Main Project/Management/Organizer/MVC/php/createEvent.php
@@ -6,3 +6,12 @@ if (!isset($_SESSION['id'])) {
     header("Location: ../html/login.php");
     exit();
 }
+$sql = "SELECT id, fullname, email 
+        FROM `user`";
+
+$result = $conn->query($sql);
+
+if (!$result) {
+    die("Query Failed: " . $conn->error);
+}
+?>
