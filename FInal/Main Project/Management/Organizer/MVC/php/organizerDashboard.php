df84be5e5fff1e9771e882565acb8f30c4ebcf6f
rahmanmushiuraiub <mushiurrehman1610917670@gmail.com>
2026-01-10 11:12:32 +0600

    oraganizerdashboard

--- FInal/Main Project/Management/Organizer/MVC/php/organizerDashboard.php
+++ FInal/Main Project/Management/Organizer/MVC/php/organizerDashboard.php
@@ -5,3 +5,19 @@
     <link rel="stylesheet" href="../css/organizerDashboard.css">
 </head>
 <body>
+ <div class="a">
+        <h2>EventOrg</h2>
+        <nav class = "z">
+            <ul>
+                <li class="active"><a href="organizerDashboard.php">Dashboard</a></li>
+                <li><a href="createEvent.php">Create Event</a></li>
+                <li><a href="manageEvent.php">Manage Events</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Settings</a></li>
+                <li><a href="#">Payment</a></li>
+                <li><a href="logout.php">Logout</a></li>
+                
+            </ul>
+
+        </nav>
+    </div>
