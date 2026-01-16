c05549dcb9045d82cc87b3bf9e7a0af34bb4b35e
rahmanmushiuraiub <mushiurrehman1610917670@gmail.com>
2026-01-16 21:47:22 +0600

    req v4

---
+++ FInal/Main Project/Management/User/MVC/php/requestEvent.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    include "../db/db.php";
+
+    $eventName = trim($_POST['eventName']);
+    $eventDate = trim($_POST['eventDate']);
+    $eventLocation = trim($_POST['eventLocation']);
+    $eventDescription = trim($_POST['eventDescription']);
+
+    if (empty($eventName) || empty($eventDate) || empty($eventLocation) || empty($eventDescription)) {
+        die("All fields are required");
+    }
+
+    $sql = "INSERT INTO event_requests (event_name, event_date, event_location, event_description, status)
+            VALUES ('$eventName', '$eventDate', '$eventLocation', '$eventDescription', 'pending')";
+
+    
+
+    $conn->close();
+}
