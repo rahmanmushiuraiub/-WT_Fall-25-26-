61cd23962abd740c6861a750b57cbbd2d6015c17
rahmanmushiuraiub <mushiurrehman1610917670@gmail.com>
2026-01-16 21:47:49 +0600

    reqv5

--- FInal/Main Project/Management/User/MVC/php/requestEvent.php
+++ FInal/Main Project/Management/User/MVC/php/requestEvent.php
@@ -14,7 +14,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "INSERT INTO event_requests (event_name, event_date, event_location, event_description, status)
             VALUES ('$eventName', '$eventDate', '$eventLocation', '$eventDescription', 'pending')";
 
-    
+    if ($conn->query($sql)){
+        echo "<script>
+                alert('Event request submitted successfully! Thank you for contacting us.');
+                window.location.href='../html/userDashboard.php';
+              </script>";
+        exit();
+    } else {
+        echo "Error: " . $conn->error;
+    }
 
     $conn->close();
 }
