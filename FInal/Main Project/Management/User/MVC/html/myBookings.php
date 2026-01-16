024e2c6a69180132c9dcde5e5cdcc5a7babe1bb7
rahmanmushiuraiub <mushiurrehman1610917670@gmail.com>
2026-01-16 21:39:23 +0600

    bookingsv1

---
+++ FInal/Main Project/Management/User/MVC/html/myBookings.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <title>My Bookings</title>
+    </head>
+    <body>
+        <h2>My Bookings</h2>
+        <?php include "../php/myBookings.php"; ?>
+    </body>
+</html>
