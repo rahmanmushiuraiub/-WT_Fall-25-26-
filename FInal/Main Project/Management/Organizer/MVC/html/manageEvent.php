14a5d12f3723c16dadd432e78f94fff353a02292
rahmanmushiuraiub <mushiurrehman1610917670@gmail.com>
2026-01-18 20:17:40 +0600

    v1

--- FInal/Main Project/Management/Organizer/MVC/html/manageEvent.php
+++ FInal/Main Project/Management/Organizer/MVC/html/manageEvent.php
@@ -1,15 +1,24 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['organizer_id'])) {
+    header("Location: ../../../User/MVC/html/login.php");
+    exit();
+}
+
+$organizerName = isset($_SESSION['organizer_name']) ? $_SESSION['organizer_name'] : 'Organizer';
+?>
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Pending Event Requests</title>
-    <link rel="stylesheet" href="../css/manageEvent.css">
-</head>
+
 <body>
-    
 
-    <h2>Pending Event Requests</h2>
 
-    
-    <?php include "../php/manageEvent.php"; ?>
+    <div class="main-content">
+        
+        
+        <?php include "../php/manageEvent.php"; ?>
+    </div>
+</div>
+
 </body>
 </html>
